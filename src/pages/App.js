e356df6ec28207e4b81b8e4d7ef5abce667a5e18
Kyzure <Kyzurene@gmail.com>
2021-11-26 17:27:24 +0800

    Added: Header

---
+++ src/pages/App.js
@@ -0,0 +1,15 @@
+import * as React from 'react';
+import '../styles/App.css';
+import Header from '../components/Header.js';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header/>
+      </header>
+    </div>
+  );
+}
+
+export default App;
